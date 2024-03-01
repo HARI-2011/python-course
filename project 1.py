9eb400b05cda13ad201e9edaaca688636ed76e79
HARI-2011 <161855673+HARI-2011@users.noreply.github.com>
2024-03-01 20:35:02 +0530

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,12 @@
+# simulating the traffic light using if else elif statements
+
+light=input('enter the light:')
+if(light=='red'):
+    print('red means stop')
+elif(light=='yellow'):
+    print('yellow means slow down')
+elif(light== 'green'):
+    print( 'green means go')
+else:
+    print('invalid color')
+
